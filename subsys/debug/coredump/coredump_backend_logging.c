1c3f755407c55b315d72efd3103bc5ddad77af92
Laurynas Lisauskas <l.lisauskas@gmail.com>
2021-04-25 14:26:22 +0300

    modules: coredump: fix logging backend prints

--- subsys/debug/coredump/coredump_backend_logging.c
+++ subsys/debug/coredump/coredump_backend_logging.c
@@ -14,10 +14,10 @@
 LOG_MODULE_REGISTER(coredump, CONFIG_KERNEL_LOG_LEVEL);
 
 /* Length of buffer of printable size */
-#define LOG_BUF_SZ		64
+#define LOG_BUF_SZ 64
 
 /* Length of buffer of printable size plus null character */
-#define LOG_BUF_SZ_RAW		(LOG_BUF_SZ + 1)
+#define LOG_BUF_SZ_RAW (LOG_BUF_SZ + 1)
 
 /* Log Buffer */
 static char log_buf[LOG_BUF_SZ_RAW];
@@ -70,7 +70,7 @@ static void coredump_logging_backend_buffer_output(uint8_t *buf, size_t buflen)
 
 		if ((log_ptr >= LOG_BUF_SZ) || (remaining == 0)) {
 			log_buf[log_ptr] = '\0';
-			LOG_ERR(COREDUMP_PREFIX_STR "%s", log_buf);
+			LOG_ERR(COREDUMP_PREFIX_STR "%s", log_strdup(log_buf));
 			log_ptr = 0;
 		}
 	}
@@ -93,8 +93,7 @@ static int coredump_logging_backend_query(enum coredump_query_id query_id,
 	return ret;
 }
 
-static int coredump_logging_backend_cmd(enum coredump_cmd_id cmd_id,
-					void *arg)
+static int coredump_logging_backend_cmd(enum coredump_cmd_id cmd_id, void *arg)
 {
 	int ret;
 
@@ -111,7 +110,6 @@ static int coredump_logging_backend_cmd(enum coredump_cmd_id cmd_id,
 	return ret;
 }
 
-
 struct z_coredump_backend_api z_coredump_backend_logging = {
 	.start = coredump_logging_backend_start,
 	.end = coredump_logging_backend_end,
@@ -120,12 +118,11 @@ struct z_coredump_backend_api z_coredump_backend_logging = {
 	.cmd = coredump_logging_backend_cmd,
 };
 
-
 #ifdef CONFIG_DEBUG_COREDUMP_SHELL
 #include <shell/shell.h>
 
-static int cmd_coredump_error_get(const struct shell *shell,
-				  size_t argc, char **argv)
+static int cmd_coredump_error_get(const struct shell *shell, size_t argc,
+				  char **argv)
 {
 	ARG_UNUSED(argc);
 	ARG_UNUSED(argv);
@@ -139,8 +136,8 @@ static int cmd_coredump_error_get(const struct shell *shell,
 	return 0;
 }
 
-static int cmd_coredump_error_clear(const struct shell *shell,
-				    size_t argc, char **argv)
+static int cmd_coredump_error_clear(const struct shell *shell, size_t argc,
+				    char **argv)
 {
 	error = 0;
 
@@ -150,16 +147,17 @@ static int cmd_coredump_error_clear(const struct shell *shell,
 }
 
 SHELL_STATIC_SUBCMD_SET_CREATE(sub_coredump_error,
-	SHELL_CMD(get, NULL, "Get Coredump error", cmd_coredump_error_get),
-	SHELL_CMD(clear, NULL, "Clear Coredump error",
-		  cmd_coredump_error_clear),
-	SHELL_SUBCMD_SET_END /* Array terminated. */
+			       SHELL_CMD(get, NULL, "Get Coredump error",
+					 cmd_coredump_error_get),
+			       SHELL_CMD(clear, NULL, "Clear Coredump error",
+					 cmd_coredump_error_clear),
+			       SHELL_SUBCMD_SET_END /* Array terminated. */
 );
 
 SHELL_STATIC_SUBCMD_SET_CREATE(sub_coredump,
-	SHELL_CMD(error, &sub_coredump_error,
-		  "Get/clear backend error.", NULL),
-	SHELL_SUBCMD_SET_END /* Array terminated. */
+			       SHELL_CMD(error, &sub_coredump_error,
+					 "Get/clear backend error.", NULL),
+			       SHELL_SUBCMD_SET_END /* Array terminated. */
 );
 
 SHELL_CMD_REGISTER(coredump, &sub_coredump,
